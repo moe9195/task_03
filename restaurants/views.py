fc1612f5e2d9f827ca7fad00ddce0f0770434308
moe9195 <moerahmeh95@hotmail.com>
2020-02-12 11:04:30 +0200

    ...

--- restaurants/views.py
+++ restaurants/views.py
@@ -5,15 +5,28 @@ def welcome(request):
 
 def restaurant_list(request):
 
-    context = {
+    #context = {'my_list' : [{'restaurant_name': ['Pizza Hut', 'Dunkin Donuts', 'KFC']},
+    #                        {'food_type': ['Pizza', 'Donuts', 'Fried Chicken'}]]}
 
-    }
+
+    context = {'my_list':
+                        [{'restaurant_name': 'kfc',
+                         'food_type': 'chicken'},
+
+                         {'restaurant_name': 'pizza hut',
+                         'food_type': 'pizza'},
+
+                         {'restaurant_name': 'dunkin donuts',
+                          'food_type': 'donuts'}
+                                            ]}
     return render(request, 'list.html', context)
 
 
 def restaurant_detail(request):
 
-    context = {
+    context = {'my_object':
+                            {'restaurant_name': 'pizza hut',
+                             'food_type': 'pizza'}
 
     }
     return render(request, 'detail.html', context)
